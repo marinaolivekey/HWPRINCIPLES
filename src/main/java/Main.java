dedfd8120bad9b489210500c64687ed7693e447b
Marina Kononova <marinakononova@mail.ru>
2024-03-14 01:00:19 +0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Post post = new Post();
+
+        post.name = "Евгений";
+        post.passport= "4444 № 444444";
+        post.patronymic = "Олегович";
+        post.phone = "+7(999) 999-99-99";
+        post.surname = "Смирнов";
+        post.subscription = true;
+
+
+    }
+
+}
