cd5c1785595e06a302c5ae0cd5a01f509e62ce1e
Marina Kononova <marinakononova@mail.ru>
2024-03-14 01:39:31 +0400

    Last code version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,16 @@ public class Main {
         Post post = new Post();
 
         post.name = "Евгений";
-        post.passport= "4444 № 444444";
+        post.passport = "4444 № 444444";
         post.patronymic = "Олегович";
         post.phone = "+7(999) 999-99-99";
         post.surname = "Смирнов";
         post.subscription = true;
 
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 12;
+        post.birthday.year = 1999;
 
     }
 
